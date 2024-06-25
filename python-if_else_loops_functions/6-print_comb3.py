7ab2d072b98509992529c1a050f33919b4352eae
Sougnabe <s.payang@alustudent.com>
2024-06-25 15:23:03 -0700

    ok

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i < 9 or j < 9:
-            print("{:02d}{:02d}, ".format(i, j), end="")
+            print("{:d}{:d}, ".format(i, j), end="")
         else:
-            print("{:02d}{:02d}".format(i, j))
+            print("{:d}{:d}".format(i, j))
 
