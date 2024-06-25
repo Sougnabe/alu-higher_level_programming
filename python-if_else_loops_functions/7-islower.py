2bcc3ec16d89286acfc9b2a29998ec4e835deca4
Sougnabe <s.payang@alustudent.com>
2024-06-25 16:17:07 -0700

    ok

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    # Vérifie si le caractère c est en minuscule
-    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+    value = ord(c)
+    if value >= 97 and value <= 122:
         return True
-    else:
+    else
         return False
-if __name__ == "__main__":
-    print("a is {}".format("lower" if islower("a") else "upper"))
-    print("H is {}".format("lower" if islower("H") else "upper"))
-    print("A is {}".format("lower" if islower("A") else "upper"))
-    print("3 is {}".format("lower" if islower("3") else "upper"))
-    print("g is {}".format("lower" if islower("g") else "upper"))
