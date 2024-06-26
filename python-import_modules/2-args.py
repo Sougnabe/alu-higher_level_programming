b742b91afad29518e571e21a21c1004eeb1c119c
Sougnabe <s.payang@alustudent.com>
2024-06-26 16:11:30 -0700

    ok

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-if "__name__" == "__main__":
+if __name__ == "__main__":
     import sys
-    argv = sys.argv[1:] 
-    arg_count = len(argv)
-
-    if arg_count == 0:
-        print("Number of argument(s): 0.")
-    elif arg_count == 1:
-        print("Number of argument(s): 1 argument:")
-    else:
-        print(f"Number of argument(s): {arg_count} arguments:")
-
-    for i, arg in enumerate(argv, start=1):
-        print(f"{i}: {arg}")
+    values = sys.argv
+    length = len(values)
+    i = 1
+    if length == 1:
+        print("0 arguments.")
+    elif length == 2:
+        print("1 argument:")
+        print("1: {}".format(values[1]))
+    elif length > 2:
+        print("{:d} arguments:".format(number-1))
+        while i < number:
+            print("{:d}: {}".format((i), values[i]))
+            i = i + 1
