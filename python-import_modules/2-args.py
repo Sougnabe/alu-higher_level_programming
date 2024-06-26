83efb8d9a0ab0e8cffa8851bcb470bf684a81cbc
Sougnabe <s.payang@alustudent.com>
2024-06-26 16:03:50 -0700

    ok

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if "__name__" == "_main_":
+    import sys
+    argv = sys.argv[1:] 
+    arg_count = len(argv)
+
+    if arg_count == 0:
+        print("Number of argument(s): 0.")
+    elif arg_count == 1:
+        print("Number of argument(s): 1 argument:")
+    else:
+        print(f"Number of argument(s): {arg_count} arguments:")
+
+    for i, arg in enumerate(argv, start=1):
+        print(f"{i}: {arg}")
