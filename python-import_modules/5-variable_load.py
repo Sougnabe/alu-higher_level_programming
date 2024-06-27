d42a7510c6eb11470f6d1565bfdf338e6006b4ee
Sougnabe <s.payang@alustudent.com>
2024-06-26 17:27:01 -0700

    ok

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import variable_load_5.py
-for x in dir(variable_load_5.py):
+    import variable_load_5
+for x in dir(variable_load_5):
     if x [0] == a:
         print("{:s}".format(x))
