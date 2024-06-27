4400e321f4bea8324c803f71d538fb4a236c3a1d
Sougnabe <s.payang@alustudent.com>
2024-06-27 15:47:57 -0700

    ok

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]])
+def print_matrix_integer(matrix=[[]]):
         counter = 0
         for i in record:
             if counter == (len(i) - 1):
