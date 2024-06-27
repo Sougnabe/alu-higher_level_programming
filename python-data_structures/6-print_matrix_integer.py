d4885eefc1c9d3d1169992b83036db7868698773
Sougnabe <s.payang@alustudent.com>
2024-06-27 15:45:42 -0700

    ok

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]])
+        counter = 0
+        for i in record:
+            if counter == (len(i) - 1):
+                print("{:d}".format(i), end="")
+            else:
+                print("{:d}".format(i), end=" ")
+            counter += 1
+        print("")
