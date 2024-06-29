8602104c571b1862b95711eef8bd6d8d2c09eb41
Sougnabe <s.payang@alustudent.com>
2024-06-29 08:04:13 -0700

    ok

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if not my_list:
-        return 0
+        return "Av: 0.00"
 
     numerator = sum(score * weight for score, weight in my_list)
     denominator = sum(weight for _, weight in my_list)
-    
-    return numerator / denominator if denominator != 0 else 0
+
+    average = numerator / denominator if denominator != 0 else 0
+    return f"Av: {average:.2f}"
 
 # Test the function
-print(weight_average([(10, 2), (20, 1), (30, 3)]))  # Output: 20.0
-print(weight_average([]))  # Output: 0
+print(weight_average([(10, 2), (20, 1), (30, 3)]))  # Output: Av: 20.00
+print(weight_average([]))  # Output: Av: 0.00
