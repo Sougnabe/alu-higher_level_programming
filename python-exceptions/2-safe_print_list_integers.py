11caae66a15c208ba8f6f263deb5aac67bf2954b
Sougnabe <s.payang@alustudent.com>
2024-07-05 16:33:30 -0700

    ok

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except TypeError:
             pass
+        except ValueError:
+            pass
         finally:
             return count
 
