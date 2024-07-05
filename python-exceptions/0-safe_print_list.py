575ff5865884834c4529ed63d1a402607d77216f
Sougnabe <s.payang@alustudent.com>
2024-07-05 05:48:30 -0700

    ok

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    try:
-        while count < x:
-            print(my_list[count], end='')
-            count += 1
+    start = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            start += 1
         except IndexError:
             pass
-        finally:
-            print()
-            return count
-print(safe_print_list([1, 2, 3, 4], 4))
-print(safe_print_list([1, 2, 3, 4], 2))
-print(safe_print_list([1, 2, 3, 4], 0))
-print(safe_print_list([], 0))
-print(safe_print_list([1, 2, 3, 4], 5))
-print(safe_print_list([1, 2, 3, 4], 14))
+    print()
+    return start
