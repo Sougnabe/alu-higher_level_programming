c21e8b42ed91046659dec087f15797eaebb586d8
Sougnabe <s.payang@alustudent.com>
2024-07-05 04:25:03 -0700

    ok

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x):
-    try:
-        print("my_list[x:d]")
-    except TypeError:
-        print("my_list[x:s]")
-    except TypeError:
-        print("my_list[x:f]")
+    count = 0
+    while count < x:
+        try:
+            print('my_list[x]')
+            count += 1
+    except IndexError:
+        pass
+    finally:
+        print("count")
 
