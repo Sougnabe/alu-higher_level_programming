1b8e04d354f2b3ba3538088bf9f4826e0bf5d4ed
Sougnabe <s.payang@alustudent.com>
2024-07-05 05:02:52 -0700

    ok

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -10,3 +10,9 @@ def safe_print_list(my_list=[], x=0):
         finally:
             print()
             return count
+print(safe_print_list([1, 2, 3, 4], 4))
+print(safe_print_list([1, 2, 3, 4], 2))
+print(safe_print_list([1, 2, 3, 4], 0))
+print(safe_print_list([], 0))
+print(safe_print_list([1, 2, 3, 4], 5))
+print(safe_print_list([1, 2, 3, 4], 14))
