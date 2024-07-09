4067f14880bf2a1b73756410f58aea125ebed7ff
Sougnabe <s.payang@alustudent.com>
2024-07-09 06:44:04 -0700

    ok

---
+++ python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+This following classe is empty
+"""
+
+
+class Square():
+    """
+This class doesn't have modules
+    """
+    def __init__(self, size=0):
+        """
+        Now the class has a module
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size**2
+    @proprety
+    def size(self):
+        """
+        the size getter
+        """
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """
+        the size setter
+        """
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
