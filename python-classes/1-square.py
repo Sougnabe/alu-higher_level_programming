0d5ff360f23f76df0e473fe496927519504f1fcb
Sougnabe <s.payang@alustudent.com>
2024-07-09 05:37:12 -0700

    ok

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This following classe is empty
+"""
+
+
+class Square():
+    """
+    This class doesn't have modules
+    """
+    def __init__(self, size):
+        self.__size = size
