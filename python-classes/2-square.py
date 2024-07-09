11633cb5e3fdea06fdc8d501cd7a461442c27cae
Sougnabe <s.payang@alustudent.com>
2024-07-09 05:44:36 -0700

    ok

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+This following classe is empty
+"""
+
+
+class Square():
+    """
+    This class doesn't have modules
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if size is not int:
+            raise TypeError"size must be an integer"
+        elif size < 0:
+            raise TypeError"size must be >= 0"
