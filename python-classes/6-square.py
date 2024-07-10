00dcf9e935cac9236b3db89cf1715bd4bc6d5cbe
Sougnabe <s.payang@alustudent.com>
2024-07-10 01:31:06 -0700

    ok

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -25,12 +25,14 @@ This class doesn't have modules
         give the erea of teh current square
         """
         return self.__size**2
+
     @property
     def size(self):
         """
         the size getter
         """
         return self.__size
+
     @size.setter
     def size(self, value):
         """
@@ -50,14 +52,12 @@ This class doesn't have modules
         else:
             for _ in range(self.__size):
                 print("#" * self.__size)
-    @property
-    def position(self):
-        return self.__position
-    @position.settter
-    def position(self, value=(0, 0)):
-        if not isinstance(value, tuple):
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif value < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        self.__position = value
