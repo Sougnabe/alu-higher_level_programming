1e294467b2591d0174dc474d9667a19d1d3cfac4
Sougnabe <s.payang@alustudent.com>
2024-07-10 06:03:44 -0700

    ok

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
+""" no module importation"""
 class Rectangle():
     """
     a rectangle has four sides
     """
     def __init__(self):
+        """ init module """
         pass
