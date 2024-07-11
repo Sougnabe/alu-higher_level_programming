bbbaf87572b607c3cd86caf2e6764f781bb98b45
Sougnabe <s.payang@alustudent.com>
2024-07-11 01:25:05 -0700

    ok

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -7,11 +7,11 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """
-        init methide 
+        init methode 
         """
         
-        self.width = width
-        self.height = height
+        self.__width = width
+        selfi.__height = height
 
     @property
     def width(self):
