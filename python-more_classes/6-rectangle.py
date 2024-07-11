f7f20e9aac5a0ddbb6865eb27ea9ba2a8405d5f5
Sougnabe <s.payang@alustudent.com>
2024-07-11 08:14:20 -0700

    ok

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
         """
         self.__width = width
         self.__height = height
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
         
     @property
     def width(self):
@@ -67,4 +67,4 @@ class Rectangle:
     def __del__(self):
         """message after deleting a instance"""
         print("Bye rectangle...")
-        number_of_instances += 1
+        Rectangle.number_of_instances -= 1
