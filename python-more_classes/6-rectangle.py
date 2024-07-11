03ae7879bb8be8e22d1e3d95150f0872cefd1b83
Sougnabe <s.payang@alustudent.com>
2024-07-11 08:27:39 -0700

    ok

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """Represents a rectangle with his characteristics"""
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0, number_of_instances=0):
         """
