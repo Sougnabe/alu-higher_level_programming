d0308ef5b5064526867a58241418b16373622e14
Sougnabe <s.payang@alustudent.com>
2024-07-17 14:49:05 -0700

    ok

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, encodng="utf8") as f:
+    with open(filename, encodng="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
