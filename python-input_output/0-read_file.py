5395e56905066a5ea0d13eb5074c90b2d15acf78
Sougnabe <s.payang@alustudent.com>
2024-07-17 14:57:21 -0700

    ok

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 '''a python script which read a file'''
 def read_file(filename=""):
     """reads the file given"""
-    with open(filename, encodng="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
