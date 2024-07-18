66bff97bbe11b6a8720a76ef8422de73a95cc84a
Sougnabe <s.payang@alustudent.com>
2024-07-18 15:28:19 -0700

    ok

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""script which returns an object (Python data structure) represented by a JSON string"""
+"""returns an object represented by a JSON string"""
 
 
 import json
 
 
 def from_json_string(my_str):
-    """function that returns an object (Python data structure) represented by a JSON string:"""
+    """function returns an object represented by a JSON string"""
     return json.loads(my_str)
--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """foction that writes an Object to a text file, using a JSON representation:"""
+    """writes Object to a text file, using a JSON representation:"""
     with open(filename, 'w+') as f:
         return json.dump(my_obj, f)
