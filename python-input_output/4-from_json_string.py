acf77f8930e346af26d6101ca38090e9da0ce04a
Sougnabe <s.payang@alustudent.com>
2024-07-18 02:39:14 -0700

    ok

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -7,4 +7,4 @@ import json
 
 def from_json_string(my_str):
     """function that returns an object (Python data structure) represented by a JSON string:"""
-    return json.dumbs(my_str)
+    return json.loads(my_str)
