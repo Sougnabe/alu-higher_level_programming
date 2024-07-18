3399954b3d7a1aee803e5273bb16c47a31544e9f
Sougnabe <s.payang@alustudent.com>
2024-07-18 15:24:55 -0700

    ok

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -8,4 +8,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """foction that writes an Object to a text file, using a JSON representation:"""
     with open(filename, 'w+') as f:
-        return f.dump(my_obj, f)
+        return json.dump(my_obj, f)
--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ import json
 def load_from_json_file(filename):
     """a function that creates an Object from a “JSON file”"""
     with open(filename, 'r') as f:
-        return json.load(filename)
+        return json.load(f)
