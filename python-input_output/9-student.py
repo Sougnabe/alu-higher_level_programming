f84d242b2299043945af483c6e13a25258d3d09d
Sougnabe <s.payang@alustudent.com>
2024-07-19 04:29:27 -0700

    ok

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-'''good'''
+'''class that makes a student'''
 
-class student:
-    '''goood'''
+
+class Student:
+    '''student class'''
     first_name = None
     last_name = None
     age = None
 
     def __init__(self, first_name, last_name, age):
-        '''goood'''
+        '''inits the data needed'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        '''good'''
+        '''returns a dict rep of the data'''
         context = {
             'first_name': self.first_name,
             'last_name': self.last_name,
