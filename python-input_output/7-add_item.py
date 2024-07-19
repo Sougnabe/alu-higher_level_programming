ed64d92c198c33c4a15e69f3cdd64d3867a114c0
Sougnabe <s.payang@alustudent.com>
2024-07-19 04:03:31 -0700

    ok

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,13 +4,13 @@
 import sys
 import os
 
-# Import the necessary functions from the previous files
+"""Import the necessary functions from the previous files"""
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 try:
-    # Load existing data from the file if it exists
+    """ Load existing data from the file if it exists"""
     if os.path.exists(filename):
         my_list = load_from_json_file(filename)
     else:
@@ -18,8 +18,8 @@ try:
 except Exception:
     my_list = []
 
-# Add all command-line arguments to the list
+"""Add all command-line arguments to the list"""
 my_list.extend(sys.argv[1:])
 
-# Save the updated list back to the file
+""" Save the updated list back to the file"""
 save_to_json_file(my_list, filename)
