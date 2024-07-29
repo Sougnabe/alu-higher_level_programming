3af023fff54b0186398e43955ac4876234a09466
Sougnabe <s.payang@alustudent.com>
2024-07-29 09:09:12 +0000

    ok

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""url module imported"""
+"""goood url importation """
 
 
 import urllib.request
 
 
-url = 'https://alu-intranet.hbtn.io/status'
-
 if __name__ == '__main__':
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-
-print("Body response:")
-print("\t- type:", type(body))
-print("\t- content:", body)
-print("\t- utf8 content:", body.decode('utf-8'))
-
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
