96bee1e0074a84d9c61ef12863f3d763ffcd1fb7
Sougnabe <s.payang@alustudent.com>
2024-08-07 11:20:28 +0000

    ok

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,12 +8,24 @@ import urllib.request
 
 if __name__ == '__main__':
     url = 'https://intranet.hbtn.io/status'
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
+    }
     req = urllib.request.Request(url, headers=headers)
     
-    with urllib.request.urlopen(req) as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+    try:
+        with urllib.request.urlopen(req) as response:
+            content = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- utf8 content: {}".format(content.decode("utf-8")))
+    except urllib.error.HTTPError as e:
+        print(f"HTTP Error: {e.code}")
+    except urllib.error.URLError as e:
+        print(f"URL Error: {e.reason}")
+
