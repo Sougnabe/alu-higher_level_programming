195ae85799a16ad1ba15fae68055cf76fa8cdfdd
Sougnabe <s.payang@alustudent.com>
2024-08-07 11:25:47 +0000

    ok

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,35 +1,14 @@
 #!/usr/bin/python3
-"""
-A Python script that fetches https://intranet.hbtn.io/status.
-"""
+"""It's a working module"""
+
 
 import urllib.request
 
-def fetch_status(url):
-    """
-documented
-    """
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1'
-    }
-    req = urllib.request.Request(url, headers=headers)
-
-    try:
-        with urllib.request.urlopen(req) as response:
-            content = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode("utf-8")))
-    except urllib.error.HTTPError as e:
-        print(f"HTTP Error: {e.code}")
-    except urllib.error.URLError as e:
-        print(f"URL Error: {e.reason}")
 
 if __name__ == '__main__':
-    fetch_status('https://intranet.hbtn.io/status')
-
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
