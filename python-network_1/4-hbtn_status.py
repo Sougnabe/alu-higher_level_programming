521a7f0aca6289cc6f7935a650b3be99362833d9
Sougnabe <s.payang@alustudent.com>
2024-08-07 11:50:07 +0000

    ok

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-documented
+A Python script that fetches a status from a specified URL.
 """
 
 import requests
@@ -15,26 +15,14 @@ def fetch_status(url):
     Returns:
         None
     """
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raises HTTPError for bad responses
-        content = response.text.strip()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-    except requests.exceptions.RequestException as err:
-        print(f"Error occurred: {err}")
+    response = requests.get(url)
+    content = response.text.strip()
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
 
 if __name__ == '__main__':
-    # URLs to fetch from
-    urls = [
-        'https://intranet.hbtn.io/status',
-        'http://0.0.0.0:5050/status'
-    ]
-    
-    for url in urls:
-        print(f"Fetching status from: {url}")
-        fetch_status(url)
-        print()
+    # Uncomment the URL you want to fetch
+    # url = 'https://intranet.hbtn.io/status'
+    url = 'http://0.0.0.0:5050/status'
+    fetch_status(url)
