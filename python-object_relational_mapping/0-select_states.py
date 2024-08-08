dea805cf381bb12537e8396b7413893f6f08ea8c
Sougnabe <s.payang@alustudent.com>
2024-08-08 20:13:32 +0000

    ok

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a function that establishes a connection to a SQL server"""
+"""Documented"""
 
 import MySQLdb
 import sys
--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,15 @@
 #!/usr/bin/python3
-"""This module defines a function that creates a connection to a SQL server"""
-
+"""
+displays all values in the states table where name matches the argument.
+"""
 import MySQLdb
 import sys
 
-
-def match_state(username, password, dbase, searched):
-    """This function imports a database to use"""
-
-    """Establish connection"""
-    db = MySQLdb.connect(host="localhost",
-                         user=username,
-                         passwd=password,
-                         database=dbase)
-
-    """Create cursor object"""
-    cur = db.cursor()
-    cur.execute("SELECT * FROM {0} ORDER BY {0}.id ASC;".format(searched))
-
-    """Access the queried data to print"""
-    rows = cur.fetchall()
-    for row in rows:
-        if row[1] == searched:
-            print(row)
-
-    """Close the connection and cursor object"""
-    cur.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    match_state(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+if __name__ == '__main__':
+    con = MySQLdb.connect(db=sys.argv[3], user=sys.argv[1], passwd=sys.argv[2])
+    with con.cursor() as cur:
+        """Used context manager to automatically close the cursor object"""
+        query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id;"
+        cur.execute(query.format(sys.argv[4]))
+        [print(row) for row in cur.fetchall()]
+    con.close()
