ebfe6866d44cbda4f060a0fef7e7b95af9f44e13
Sougnabe <s.payang@alustudent.com>
2024-08-08 19:14:41 +0000

    ok

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
+"""__summary__
+- Write a Python script that
+- fetches https://intranet.hbtn.io/status.
 """
-A Python script that fetches a status from a specified URL.
-"""
-
 import requests
 
-def fetch_status(url):
-    """
-    Fetches the status from a given URL and prints the response details.
-    
-    Args:
-        url (str): The URL to fetch the status from.
-    
-    Returns:
-        None
-    """
-    response = requests.get(url)
-    content = response.text.strip()
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
 
-if __name__ == '__main__':
-    urls = [
-        'https://intranet.hbtn.io/status',
-        'http://0.0.0.0:5050/status'
-    ]
-    
-    for url in urls:
-        fetch_status(url)
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
