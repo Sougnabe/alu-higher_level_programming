92cb2e2d07a5c71215b4b672eab146cc546afea2
Sougnabe <s.payang@alustudent.com>
2024-10-01 23:43:02 +0000

    ok

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 const arg = process.argv[2];
-fs.readFile(arg, 'utf-8' , (err, data) => {
-if (err){
-console.log(err);
-} else {
-console.log(data);
-}
+fs.readFile(arg, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
-
