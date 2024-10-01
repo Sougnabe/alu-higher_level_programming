09faa2153aaaefa4b96832a1076d4e6f149dd82c
Sougnabe <s.payang@alustudent.com>
2024-10-01 23:40:50 +0000

    ok

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+const arg = process.argv[2];
+let data = process.argv[3];
+fs.writeFile(arg, data,  'utf-8', (err) => {
+if (err) {
+console.log(err);
+});
