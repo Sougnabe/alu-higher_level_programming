2a946199232831314af999a0e615ac6d9e66d50a
Sougnabe <s.payang@alustudent.com>
2024-10-02 23:15:57 +0000

    ok

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 const arg = process.argv[2];
-let data = process.argv[3];
-fs.writeFile(arg, data,  'utf-8', (err) => {
-if (err) {
-console.log(err);
-}
+const data = process.argv[3];
+fs.writeFile(arg, data, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
 });
--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,20 @@
 #!/usr/bin/node
 
 const request = require('request');
-const apiUrl = process.argv[2]; 
+const url = process.argv[2];
+let count = 0;
+let data;
 
-request(apiUrl, (err, response, body) => {
+request.get(url, function (err, res) {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    films.forEach(film => {
-      if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-        count++;
-      }
+    data = JSON.parse(res.body).results;
+    data.forEach((obj) => {
+      obj.characters.forEach((character) => {
+        if (character.includes('/18/')) count++;
+      });
     });
-    console.log(count);
   }
+  console.log(count);
 });
